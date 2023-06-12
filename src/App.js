f712d9e7a5d180381e20e69778bfbbf18b289501
MateuszGorski1 <mateusz.gorski12345@wp.pl>
2023-06-12 09:30:27 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { IoIosAdd, IoIosList } from 'react-icons/io';
 import BookForm from './components/BookForm';
 import BookList from './components/BookList';
 import './styles.css';
-import env from "@beam-australia/react-env";
 
 const App = () => {
   return (
